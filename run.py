6ff2fbae3dee2a790dff18de0c044e8b07b0a878
MengtingWu <mw2987@columbia.edu>
2015-11-06 16:48:22 -0500

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from APITest import request_bing_result
-
-siteInput = "yahoo.com"
-queryInput = "avi file"
-
-webTotal = request_bing_result(siteInput, queryInput)
-import pdb; pdb.set_trace()
-print webTotal
